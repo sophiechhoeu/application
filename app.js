43e776328e55cff30f6edb1fe1cfbcb9a33021ba
sophie.chhoeu <sophie.chhoeu@carsguide.com.au>
2018-03-28 11:54:44 +1100

    test

--- app.js
+++ app.js
@@ -24,16 +24,16 @@ app.use('/tvshows', tvshowsRouter);
 
 app.get('/', (req, res) => {
   Tvshow.find()
-    .then(tvshows => res.json(tvshows))
+    .then(tvshows => res.status(200) => res.json(tvshows))
     .catch(error => res.json({ error }))
 });
 
-app.post('/',(req,res) => {
-  Tvshow.create(req.body)
-    .then((tvshow) => {
-      res.status(200).json(tvshow).end();
-    })
-    .catch(error => res.json({ error }))
-});
+// app.post('/',(req,res) => {
+//   Tvshow.create(req.body)
+//     .then((tvshow) => {
+//       res.status(200).json(tvshow).end();
+//     })
+//     .catch(error => res.json({ error }))
+// });
 
 module.exports = app
